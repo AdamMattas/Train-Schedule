0d8f97e47e644e0305f4b5aad35fe28d5997dbd1
adam fader <adamfader@gmail.com>
2016-06-19 03:16:28 -0400

    changed authentication check

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,16 @@ $(document).on('ready', function(){
 	// Firebase link
 	var dataRef = new Firebase("https://adambay.firebaseio.com/");
 	// Initial Values
-	var authData = "";
+	var authData = dataRef.getAuth();
+	
+	if(authData !== null){ //checks to see if client is authenticated
+
+		//hide login-button and show logout-button if client is authenticated
+		$("#login-button").addClass('hide').removeClass('show');
+		$("#logout-button").addClass('show').removeClass('hide');
+
+	}
+
 	var keyId = "";
 	var keyId2 = "";
 	var keyVal = "";
@@ -21,28 +30,40 @@ $(document).on('ready', function(){
 	var update = function(){
 		$('#trainData > tbody').empty(); //table body with train data is cleared
 		$('#trainData > thead > tr').empty(); //table head row is also cleared incase loggedIn state changes
-		authData = dataRef.getAuth();
-		if(authData !== null){ //checks to see if loggedIn variable is set to true
+		var authData = dataRef.getAuth();
+		if(authData !== null){ //checks to see if client is authenticated
 
 			//table head is populated with initial values plus edit and remove column titles
-			$('#trainData > thead > tr').append("<th>" + "Train Name" + "</th><th>" + "Destination" + "</th><th>" + "Frequency (min)" + "</th><th>" + "Next Arrival" + "</th><th>" + "Minutes Away" + "</th><th>" + "Remove Train" + "</th><th>" + "Edit Train" + "</th>");
+			$('#trainData > thead > tr').append("<th>" + 
+				"Train Name" + "</th><th>" + 
+				"Destination" + "</th><th>" + 
+				"Frequency (min)" + "</th><th>" + 
+				"Next Arrival" + "</th><th>" + 
+				"Minutes Away" + "</th><th>" + 
+				"Remove Train" + "</th><th>" + 
+				"Edit Train" + "</th>");
 			//queries firebase for each child in database and returns the values
 			dataRef.on("child_added", function(snapshot){
 
 				//set variable as train start time in military time format
 				startConverted = moment(snapshot.val().start,"HH:mm");
+				console.log(startConverted);
 
 				//set diffTime as the difference between the current time and start time in minutes
 				diffTime = moment().diff(startConverted, "minutes");
+				console.log(diffTime);
 
 				//returns the division remainder of diffTime and train frequency
 				tRemainder = diffTime % snapshot.val().frequency;
+				console.log(tRemainder);
 
 				//figures time until next train by subtracting frequency from tRemainder variable
 		    tilTrain = snapshot.val().frequency - tRemainder;
+		    console.log(tilTrain);
 
 		    //sets nextTrain arrival in minutes
 		    nextTrain = moment().add(tilTrain, "minutes");
+		    console.log(nextTrain);
 		   	
 		   	//table body values are populated with firebase snapshot values and train times converted above
 		   	//since logged in, there is also an edit and remove button added for each train
@@ -54,30 +75,46 @@ $(document).on('ready', function(){
 		    	//remove and edit buttons have a data-id attribute with the value of the corresponding firebase key to target the correct child in database
 		    	//edit button has a data-key attribute containing the entire snapshot of the child values
 		    	//edit button triggers a modal and uses the data-key attribute to populate the modal inputs with the current firebase values
-		    	tilTrain + "</td><td><button class=" + "remove" + "data-id=" + snapshot.key() + ">Remove</button></td><td><button class=" + "edit" + " data-toggle=" + "modal" + " data-target=" + "#myModal" + " data-key=" + JSON.stringify(snapshot.val()) + " data-id=" + snapshot.key() + ">Edit</button></td></tr>");
+		    	tilTrain + "</td><td><button class=" + "remove" + " data-id=" + snapshot.key() + ">Remove</button></td><td><button class=" + "edit" + " data-toggle=" + "modal" + " data-target=" + "#myModal" + " data-key=" + JSON.stringify(snapshot.val()) + " data-id=" + snapshot.key() + ">Edit</button></td></tr>");
 			})
 		}else{ //runs if loggedIn variable is set to false
-			$('#trainData > thead > tr').append("<th>" + "Train Name" + "</th><th>" + "Destination" + "</th><th>" + "Frequency (min)" + "</th><th>" + "Next Arrival" + "</th><th>" + "Minutes Away" + "</th>");
+			$('#trainData > thead > tr').append("<th>" + 
+				"Train Name" + "</th><th>" + 
+				"Destination" + "</th><th>" + 
+				"Frequency (min)" + "</th><th>" + 
+				"Next Arrival" + "</th><th>" + 
+				"Minutes Away" + "</th>");
 			//queries firebase for each child in database and returns the values
 			dataRef.on("child_added", function(snapshot){
 			
 				//set variable as train start time in military time format
 				startConverted = moment(snapshot.val().start,"HH:mm");
+				console.log(startConverted);
 
 				//set diffTime as the difference between the current time and start time in minutes
 				diffTime = moment().diff(startConverted, "minutes");
+				console.log(diffTime);
 
 				//returns the division remainder of diffTime and train frequency
 				tRemainder = diffTime % snapshot.val().frequency;
+				console.log(tRemainder);
 
 				//figures time until next train by subtracting frequency from tRemainder variable
 		    tilTrain = snapshot.val().frequency - tRemainder;
+		    console.log(tilTrain);
 
 		    //sets nextTrain arrival in minutes
 		    nextTrain = moment().add(tilTrain, "minutes");
+		    console.log(nextTrain);
 		   	
 		   	//table body values are populated with firebase snapshot values and train times converted above
-		    $('#trainData > tbody').append("<tr data-id=" + snapshot.key() + "><td>" + snapshot.val().trainName + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + nextTrain.format("HH:mm") + "</td><td>" + tilTrain + "</td></tr>");
+		    $('#trainData > tbody').append("<tr data-id=" + 
+		    	snapshot.key() + "><td>" + 
+		    	snapshot.val().trainName + "</td><td>" + 
+		    	snapshot.val().destination + "</td><td>" + 
+		    	snapshot.val().frequency + "</td><td>" + 
+		    	nextTrain.format("HH:mm") + "</td><td>" + 
+		    	tilTrain + "</td></tr>");
 			})
 		}
 		
@@ -85,7 +122,7 @@ $(document).on('ready', function(){
 
 	//calls update function when document is ready
 	//it is most useful when setInterval is long enough to delay initial table data population
-	update(); //<---update function is not 100% necessary
+	update(); //<---calling update function here is not 100% necessary
   setInterval(update, 1000); //calls update function every second to calculate train times
 
 	//Listens for Add Train Submit Button Click
